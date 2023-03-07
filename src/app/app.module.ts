f9d370457ae5220cb859e4acb5e9efb1ba9059eb
Vimlesh-Kumar <vimlesh11072000@gmail.com>
2023-03-07 10:13:43 +0530

    todo-app

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TodosComponent } from './mycomponents/todos/todos.component';
+import { TodoItemComponent } from './mycomponents/todo-item/todo-item.component';
+import { AddTodoComponent } from './mycomponents/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
